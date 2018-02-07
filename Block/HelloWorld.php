9cf3b9fc697a825a7ad52c5e3873900ed1ca26c9
vishal <32734144+vishalmathur@users.noreply.github.com>
2018-02-07 11:22:02 +0530

    Update and rename index.php to HelloWorld.php

---
+++ Block/HelloWorld.php
@@ -0,0 +1,14 @@
+<?php
+namespace LumbralesSoftware\HelloWorld\Block;
+use Magento\Framework\View\Element\Template;
+/**
+ * HelloWorld
+ *
+ * @package
+ * @version $id$
+ * @copyright 2015, Lumbrales Software
+ * @author Javier Carrascal <info@lumbrales-software.com>
+ */
+class HelloWorld extends Template
+{
+}
